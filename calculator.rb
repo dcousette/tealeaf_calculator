55f95ee5a594531f50d55435511635c4daca24f3
DeShawn <dcousette@gmail.com>
2015-03-02 21:51:36 +0000

    Add 'say' method update division operationto handle floats

--- calculator.rb
+++ calculator.rb
@@ -1,26 +1,31 @@
-puts "Welcome to the Calculator App! Please enter a number:"
+def say(msg)
+  puts "---------#{msg}---------"
+end
+
+
+say "Welcome to the Calculator App! Please enter a number:"
 num1 = gets.chomp 
 
 
-puts "Enter a second number:"
+say "Enter a second number:"
 num2 = gets.chomp
 
   
-puts "What would you like to do? 1) Add 2) Subtract 3) Multiply 4) Divide"
+say "What would you like to do? 1) Add 2) Subtract 3) Multiply 4) Divide"
 operation = gets.chomp
 
 begin 
-  if operation == 1.to_s
+  if operation == '1'
     result = num1.to_i + num2.to_i
     break 
-  elsif operation == 2.to_s
+  elsif operation == '2'
     result = num1.to_i - num2.to_i
     break
-  elsif operation == 3.to_s
+  elsif operation == '3'
     result = num1.to_i * num2.to_i
     break
-  elsif operation == 4.to_s
-    result = num1.to_i / num2.to_i
+  elsif operation == '4'
+    result = num1.to_f / num2.to_i
     break
   else
     puts "Please enter a valid number!"
