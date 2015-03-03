feb3d02a0366605b4b32d5534ce757b18c374b3c
DeShawn <dcousette@gmail.com>
2015-03-03 16:41:17 +0000

    Clean up loop construct - remove extra line breaks

--- calculator.rb
+++ calculator.rb
@@ -2,19 +2,14 @@ def say(msg)
   puts "---------#{msg}---------"
 end
 
-
 say "Welcome to the Calculator App! Please enter a number:"
 num1 = gets.chomp 
-
-
 say "Enter a second number:"
 num2 = gets.chomp
-
-  
 say "What would you like to do? 1) Add 2) Subtract 3) Multiply 4) Divide"
 operation = gets.chomp
 
-begin 
+loop do 
   if operation == '1'
     result = num1.to_i + num2.to_i
     break 
@@ -32,6 +27,6 @@ begin
     operation = gets.chomp 
   end
   
-end while true 
+end 
 
 puts "The result is: #{result}."
